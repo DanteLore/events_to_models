a5c878d3a6b9871fc4cfef6496b814ca72e4610f
DanteLore <dan@logicalgenetics.com>
2018-11-19 11:56:40 +0000

    Consumer now reads avro, using the schema repo

--- src/main/scala/BeerConsumer.scala
+++ src/main/scala/BeerConsumer.scala
@@ -1,6 +1,8 @@
 import java.time.Duration
 import java.util
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
 
@@ -8,20 +10,22 @@ import scala.collection.JavaConverters._
 
 object BeerConsumer {
 
-  lazy val consumer: KafkaConsumer[String, String] = {
+  val topic = "beers"
 
+  lazy val consumer: KafkaConsumer[String, GenericRecord] = {
     val properties = new util.Properties()
     properties.put("bootstrap.servers", "localhost:9092")
+    properties.put("schema.registry.url", "http://localhost:8081")
     properties.put("group.id", "cheese-group")
     properties.put("key.deserializer", classOf[StringDeserializer])
-    properties.put("value.deserializer", classOf[StringDeserializer])
+    properties.put("value.deserializer", classOf[KafkaAvroDeserializer])
     properties.put("auto.offset.reset", "earliest")
 
-    new KafkaConsumer[String, String](properties)
+    new KafkaConsumer[String, GenericRecord](properties)
   }
 
   def main(args: Array[String]): Unit = {
-    consumer.subscribe(util.Arrays.asList("test"))
+    consumer.subscribe(util.Arrays.asList(topic))
 
     var i = 0
     while (true) {
