bbfaf4e11e093d47fd2abe8c4c13ace53afb15ea
DanteLore <Dan.Taylor@thetrainline.com>
2020-05-05 09:53:24 +0100

    Got the vote logic working, limited to one vote per customer with a groupBy

---
+++ src/main/scala/com/logicalgenetics/model/Score.scala
@@ -0,0 +1,6 @@
+package com.logicalgenetics.model
+
+case class Score(beerId : Int = 0, score : Int = 0, count : Int = 1){
+  def +(that: Score): Score = Score(this.beerId, this.score + that.score, this.count + that.count)
+  def -(that: Score): Score = Score(this.beerId, this.score - that.score, this.count - that.count)
+}
--- src/main/scala/com/logicalgenetics/streams/VoteAggregatorStream.scala
+++ src/main/scala/com/logicalgenetics/streams/VoteAggregatorStream.scala
@@ -1,23 +1,22 @@
 package com.logicalgenetics.streams
 
 import java.time.Duration
-import java.util
 import java.util.Properties
 
 import com.logicalgenetics.Config
-import com.logicalgenetics.avro.{KafkaAvroCaseClassSerdes, RawAvroCaseClassSerdes}
-import com.logicalgenetics.model.Vote
+import com.logicalgenetics.avro.KafkaAvroCaseClassSerdes
+import com.logicalgenetics.model.{Score, Vote}
 import com.sksamuel.avro4s.{AvroSchema, RecordFormat}
 import org.apache.avro.Schema
-import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.common.serialization.{Serde, Serdes}
-import org.apache.kafka.streams.kstream.{Consumed, Produced}
-import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.kstream.{Consumed, Grouped, Materialized, Produced}
+import org.apache.kafka.streams.scala.{ByteArrayKeyValueStore, StreamsBuilder}
 import org.apache.kafka.streams.scala.kstream.KStream
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 
 object VoteAggregatorStream {
-  private val votes_topic = "votes"
+  private val inputTopic = "votes"
+  private val beerScoresTopic = "beer_scores"
 
   private val properties: Properties = {
     val p = new Properties()
@@ -26,28 +25,37 @@ object VoteAggregatorStream {
     p
   }
 
-  private val stringSerde: Serde[String] = Serdes.String
-  implicit val voteSchema: Schema = AvroSchema[Vote]
-  implicit val voteRF: RecordFormat[Vote] = RecordFormat[Vote]
-  private val voteSerdes: Serde[Vote] = RawAvroCaseClassSerdes()
-  //private val voteSerdes: Serde[Vote] = AvroCaseClassSerdes(Config.schemaRegistry)
+  // SO MANY implicit serdes!!
+  private val voteSerde: Serde[Vote] = {
+    implicit val schema: Schema = AvroSchema[Vote]
+    implicit val recordFormat: RecordFormat[Vote] = RecordFormat[Vote]
+    KafkaAvroCaseClassSerdes(Config.schemaRegistry)
+  }
 
-  private implicit val consumed: Consumed[String, Vote] = Consumed.`with`(stringSerde, voteSerdes)
-  private implicit val produced: Produced[String, String] = Produced.`with`(stringSerde, stringSerde)
+  private val scoreSerde: Serde[Score] = {
+    implicit val schema: Schema = AvroSchema[Score]
+    implicit val recordFormat: RecordFormat[Score] = RecordFormat[Score]
+    KafkaAvroCaseClassSerdes(Config.schemaRegistry)
+  }
+
+  implicit val consumedVotes: Consumed[String, Vote] = Consumed.`with`(Serdes.String, voteSerde)
+  implicit val groupedVotes: Grouped[String, Vote] = Grouped.`with`(Serdes.String, voteSerde)
+  implicit val groupedScores: Grouped[String, Score] = Grouped.`with`(Serdes.String, scoreSerde)
+  implicit val producedScores: Produced[String, Score] = Produced.`with`(Serdes.String, scoreSerde)
+
+  implicit val materialisedVotes : Materialized[String, Vote, ByteArrayKeyValueStore] =
+    Materialized.as("votes_by_customer")
+      .withKeySerde(Serdes.String)
+      .withValueSerde(voteSerde)
+
+  implicit val materialisedScores : Materialized[String, Score, ByteArrayKeyValueStore] =
+    Materialized.as("scores_by_beer")
+      .withKeySerde(Serdes.String)
+      .withValueSerde(scoreSerde)
 
   def main(args: Array[String]): Unit = {
-    val builder = new StreamsBuilder()
-
-    // Get the incoming votes
-    val votes: KStream[String, Vote] = builder.stream[String, Vote](votes_topic)
-
-    val customerVotes: KStream[String, String] = votes.map {
-      (_, v) => {
-        (s"${v.customerId}/${v.beerId}", s"Vote is ${v.vote}")
-      }
-    }
-
-    customerVotes.to("dan_topic")
+    // Call our method to construct the streams
+    val builder: StreamsBuilder = constructStreams
 
     // Start the streams
     val streams: KafkaStreams = new KafkaStreams(builder.build(), properties)
@@ -60,11 +68,35 @@ object VoteAggregatorStream {
     }
   }
 
-  def toCaseClass(in : GenericRecord) : Vote = {
-    val format = RecordFormat[Vote]
-    // record is a type that implements both GenericRecord and Specific Record
-    val record = format.from(in)
+  private def constructStreams: StreamsBuilder = {
+    val builder = new StreamsBuilder()
 
-    record
+    // Get the incoming votes
+    val votes: KStream[String, Vote] = builder.stream[String, Vote](inputTopic)
+
+    // Group and key the votes by customer and beer IDs
+    val customerVotes: KStream[String, Vote] = votes.map {
+      (_, v) => (s"${v.beerId}/${v.customerId}", v)
+    }
+
+    // Group by key (beerId/customerId) - this will drop duplicate votes per customerId
+    val votesByCustomerAndBeer = customerVotes
+      .groupByKey
+      .reduce((_, latest) => latest)
+
+    // Map to Scores, group by beer ID and reduce to give scores by beer
+    val beerScores = votesByCustomerAndBeer
+      .mapValues(vote => Score(vote.beerId, vote.vote))
+      .groupBy((_, score) => (score.beerId.toString, score))
+      .reduce(
+        (v1, v2) => v1 + v2,
+        (v1, v2) => v1 - v2
+      )
+
+    // Write to outgoing topic
+    beerScores.toStream.to(beerScoresTopic)
+
+    // Done :)
+    builder
   }
 }
